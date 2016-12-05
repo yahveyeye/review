736055b1af06056d391f04af81ec57be611d59eb
lin yuan <linyuan@live.cn>
2016-12-05 08:53:21 +0800

    init

---
+++ api/models/Review.js
@@ -0,0 +1,16 @@
+/**
+ * Review.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+     title: 'string',  
+     text: 'string',  
+     category: 'string' 
+  }
+};
+
